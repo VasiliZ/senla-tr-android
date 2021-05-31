3a434eacd51b7f9a81837b4758bd8dbe14cf818b
Vasili <zaicev.vasili@gmail.com>
2021-05-31 23:41:58 +0300

    first part of hw

--- app/src/main/java/com/github/rtyvz/senla/tr/android/MainActivity.kt
+++ app/src/main/java/com/github/rtyvz/senla/tr/android/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.github.rtyvz.senla.tr.android
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        for (i in 1..100) {
+            when {
+                i % 3 == 0 && i % 5 == 0 -> {
+                    Log.d(this.javaClass.canonicalName, "Fizz Buzz")
+                }
+                i % 3 == 0 -> {
+                    Log.d(this.javaClass.canonicalName, "Fizz")
+                }
+                i % 5 == 0 -> {
+                    Log.d(this.javaClass.canonicalName, "Fizz")
+                }
+                else -> {
+                    Log.d(this.javaClass.canonicalName, i.toString())
+                }
+            }
+        }
     }
 }
