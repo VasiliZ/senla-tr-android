2cf6058cbf4682325aa4c2d5fddbf6061ae2db37
Vasili <zaicev.vasili@gmail.com>
2021-06-01 15:59:55 +0300

    simple summator

--- lesson05/app/src/main/java/com/github/rtyvz/senla/tr/lesson05/MainActivity.kt
+++ lesson05/app/src/main/java/com/github/rtyvz/senla/tr/lesson05/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.github.rtyvz.senla.tr.lesson05
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.github.rtyvz.senla.tr.lesson05.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.apply {
+            calculateAction.setOnClickListener {
+
+                val firstValue = firstNumber.text.toString()
+                val secondValue = secondNumber.text.toString()
+
+                if (firstValue.isBlank() || secondValue.isBlank()) {
+                    result.text = getString(R.string.input_error)
+                } else {
+                    result.text = (firstValue.toInt() + secondValue.toInt()).toString()
+                }
+            }
+        }
     }
 }
