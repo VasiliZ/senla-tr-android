e514d6b0594bc22df16a99b6a740634c8abf3982
Vasili <zaicev.vasili@gmail.com>
2021-06-02 14:37:07 +0300

    little fix

--- lesson06/app/src/main/java/com/github/rtyvz/senla/tr/lesson_06/MainActivity.kt
+++ lesson06/app/src/main/java/com/github/rtyvz/senla/tr/lesson_06/MainActivity.kt
@@ -8,9 +8,10 @@ import com.github.rtyvz.senla.tr.lesson_06.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val userLogin = "User"
-    private val password = "!234!"
-    private var isLoginError = false
-    private var isPasswordError = false
+    private val password = "4"
+    private var isLoginError = true
+    private var isPasswordError = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -18,68 +19,62 @@ class MainActivity : AppCompatActivity() {
 
         binding.apply {
             loginAction.setOnClickListener {
-                if (passwordInput.text.isNullOrBlank()) {
-                    isPasswordError = true
-                    passwordInputLayout.error = getString(R.string.password_required)
-                } else {
-                    isPasswordError = false
-                    passwordInputLayout.error = null
-                }
 
-                if (loginInput.text.isNullOrBlank()) {
-                    isLoginError = true
-                    loginInputLayout.error = getString(R.string.login_required)
-                } else {
-                    isLoginError = false
-                    loginInputLayout.error = null
-                }
-
-                if (loginInput.text.toString() != userLogin) {
-                    isLoginError = true
-                    loginInputLayout.error = getString(R.string.wrong_login)
-                } else {
-                    isLoginError = false
-                    loginInputLayout.error = null
-                }
-
-                if (passwordInput.text.toString() != password) {
-                    isPasswordError = true
-                    passwordInputLayout.error = getString(R.string.wrong_login)
-                } else {
-                    isPasswordError = false
-                    passwordInputLayout.error = null
-                }
-
+                checkInputs()
                 if (!isLoginError && !isPasswordError) {
-                    Toast.makeText(this@MainActivity, getString(R.string.login_succes), Toast.LENGTH_SHORT).show()
+                    Toast.makeText(
+                        this@MainActivity,
+                        getString(R.string.login_succes),
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
             }
 
             registerAction.setOnClickListener {
-                if (passwordInput.text.isNullOrBlank()) {
-                    isPasswordError = true
-                    passwordInputLayout.error = getString(R.string.password_required)
-                } else {
+
+                checkInputs()
+                if (!isLoginError && !isPasswordError) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        getString(R.string.redirect),
+                        Toast.LENGTH_LONG
+                    ).show()
                     isPasswordError = false
-                    passwordInputLayout.error = null
+                    isLoginError = false
+                }
+            }
+        }
+    }
+
+    private fun checkInputs() {
+        binding.apply {
+            when {
+                loginInput.text.toString().isBlank() -> {
+                    loginInputLayout.error = getString(R.string.login_required)
                 }
 
-                if (loginInput.text.isNullOrBlank()) {
-                    isLoginError = true
-                    loginInputLayout.error = getString(R.string.login_required)
-                } else {
+                loginInput.text.toString() != userLogin -> {
+                    loginInputLayout.error = getString(R.string.wrong_login)
+                }
+
+                else -> {
                     isLoginError = false
                     loginInputLayout.error = null
                 }
+            }
 
-                if (!isLoginError && !isPasswordError) {
-                    Toast.makeText(this@MainActivity, getString(R.string.login_succes), Toast.LENGTH_SHORT).show()
+            when {
+                passwordInput.text.toString().isBlank() -> {
+                    passwordInputLayout.error = getString(R.string.password_required)
                 }
 
-                if (!isLoginError && !isPasswordError) {
-                    Toast.makeText(this@MainActivity, getString(R.string.redirect), Toast.LENGTH_LONG).show()
+                passwordInput.text.toString() != password -> {
+                    passwordInputLayout.error = getString(R.string.wrong_password)
+                }
+
+                else -> {
                     isPasswordError = false
-                    isLoginError = false
+                    passwordInputLayout.error = null
                 }
             }
         }
