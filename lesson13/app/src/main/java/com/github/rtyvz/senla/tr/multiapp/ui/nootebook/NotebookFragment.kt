810b98e1abbd1461f59292d5040e9fe604043268
Vasili <zaicev.vasili@gmail.com>
2021-06-08 22:30:18 +0300

    add back stack to notebook fragment

--- lesson13/app/src/main/java/com/github/rtyvz/senla/tr/multiapp/ui/nootebook/NotebookFragment.kt
+++ lesson13/app/src/main/java/com/github/rtyvz/senla/tr/multiapp/ui/nootebook/NotebookFragment.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.drawer.ui.nootebook.EditFileFragment
 import com.example.drawer.ui.nootebook.adapter.FilesAdapter
 import com.github.rtyvz.senla.tr.multiapp.MainActivity
@@ -110,6 +109,7 @@ class NotebookFragment : Fragment() {
         fragmentManager.fragments.clear()
         val transaction = fragmentManager.beginTransaction()
         transaction.replace(contentContainer, fragment)
+        transaction.addToBackStack(null)
         transaction.commit()
     }
 }
