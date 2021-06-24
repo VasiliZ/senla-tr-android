0f838c1e8004749cf4c4320ba69a47b3dfafc6ed
Vasili.Zaitsev <zaicev.vasili@gmail.com>
2021-06-24 14:48:07 +0300

    fix again

--- lesson20/app/src/main/java/com/github/rtyvz/senla/tr/loginapp/utils/PreferenceExt.kt
+++ lesson20/app/src/main/java/com/github/rtyvz/senla/tr/loginapp/utils/PreferenceExt.kt
@@ -2,8 +2,10 @@ package com.github.rtyvz.senla.tr.loginapp.utils
 
 import android.content.SharedPreferences
 
+private const val EMPTY_STRING = ""
+
 fun SharedPreferences.getString(key: String): String? {
-    return this.getString(key, "")
+    return this.getString(key, EMPTY_STRING)
 }
 
 fun SharedPreferences.putString(key: String, value: String) {
